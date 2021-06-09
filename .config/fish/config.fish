b13e2c88015ac1800e127c150fca23b67329b084
Luke Olfert <contact@lukeolfert.com>
2021-06-08 20:45:11 -0500

    Changed tmux autostart.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
-if status is-interactive
-and not set -q TMUX
-    exec tmux
+if not set -q TMUX
+    set -g TMUX tmux new-session -d -s base
+    eval $TMUX
+    tmux attach-session -d -t base
 end
 
 set -U fish_greeting ""
